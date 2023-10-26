7f751976381c2fa351b3a51045e6feedad9b6841
Giorgi Maisuradze <maisuradze200329@gmail.com>
2023-10-26 14:21:28 +0400

    Added Login and adding to cart only when logged in

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useReducer, useRef, useState } from 'react'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Cards from './Components/Cards'
@@ -7,17 +7,33 @@ function App() {
   const [searchInput, setSearchInput] = useState('')
   const [data, setData] = useState([])
   const [originalData, setOriginalData] = useState([]) 
-
+  const [username, setUsername ] = useState('')
+  const [password, setPassword ] = useState('')
+  console.log(username);
+  let cardAddEnabled = (localStorage.getItem("username") && localStorage.getItem("password") ? true : false)
   const fetchData = async () => {
     const res = await fetch('https://fakestoreapi.com/products')
     const d = await res.json()
     setData(d)
     setOriginalData(d)
   }
+
   useEffect(() => {
     fetchData()
   }, []);
 
+  const handleLogin = () =>{
+    if(username == "admin" && password == "admin"){
+      localStorage.setItem('username', 'admin')
+      localStorage.setItem('password', 'admin')
+      location.reload()
+    }
+  }
+  const handleLogout = () =>{
+    localStorage.removeItem('username')
+    localStorage.removeItem('password')
+    location.reload()
+  }
   const handleCategoryChange = (category) => {
     const newData = originalData.filter((dataItem) => dataItem.category === category);
     setData(newData)
@@ -38,7 +54,9 @@ function App() {
 
 
   return (
-    <div class='glavni'>  
+    <div class='container-fluid'>  
+
+    {/* Navbar  */}
     <nav class="navbar shadow-lg navbar-expand-lg navbar-light bg-light">
       <div class="container-fluid">
         <a class="navbar-brand" href="/">
@@ -83,56 +101,59 @@ function App() {
 
     <div class="">
         <div class="row m-0 ">
-          <div className="col-2 d-flex flex-column align-items-center">
 
-          <ul class="category list-group m-4">
-            <li class="list-group-item bg-primary text-white" onClick={()=>fetchData()}>Categories</li>
-            <li class="list-group-item" onClick={()=>fetchData()}>All</li>
-            <li class="list-group-item" onClick={()=>handleCategoryChange('jewelery')}>Jewelery</li>
-            <li class="list-group-item" onClick={()=>handleCategoryChange('electronics')}>Electronics</li>
-            <li class="list-group-item" onClick={()=>handleCategoryChange("men's clothing")}>Men</li>
-            <li class="list-group-item" onClick={()=>handleCategoryChange("women's clothing")}>Women</li>
-          </ul>
-          <div className="d-flex">
-            <button type="button" class="btn buttonl btn-success me-2  mt-5" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">Log In</button>
-            <button type="button" class="btn buttonl btn-danger mt-5">Log Out</button>
+          {/* Category Selection  */}
+          <div className="col-2 d-flex flex-column align-items-center">
+            {/* Categories  */}
+            <ul class="category list-group m-4">
+              <li class="list-group-item bg-primary text-white" onClick={()=>fetchData()}>Categories</li>
+              <li class="list-group-item" onClick={()=>fetchData()}>All</li>
+              <li class="list-group-item" onClick={()=>handleCategoryChange('jewelery')}>Jewelery</li>
+              <li class="list-group-item" onClick={()=>handleCategoryChange('electronics')}>Electronics</li>
+              <li class="list-group-item" onClick={()=>handleCategoryChange("men's clothing")}>Men</li>
+              <li class="list-group-item" onClick={()=>handleCategoryChange("women's clothing")}>Women</li>
+            </ul>
+
+          {/* Open Login OR Logout */}
+          <div className="d-flex flex-column">
+            <button type="button" class="btn buttonl btn-success me-2 mt-5" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">Log In</button>
+            <button type="button" class="btn buttonl btn-danger mt-5" onClick={handleLogout}>Log Out</button>
 
           </div>
           
-          <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog">
-                  <div class="modal-content">
-                    <div class="modal-header">
-                      <h1 class="modal-title fs-5" id="exampleModalLabel">Log In</h1>
-                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                      <form>
-                        <div class="form-floating mt-1 mb-3">
-                          <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" />
-                          <label for="floatingInput">Username</label>
-                        </div>
-                        <div class="form-floating">
-                          <input type="password" class="form-control" id="floatingPassword" placeholder="Password" />
-                          <label for="floatingPassword">Password</label>
-                        </div>
-                      </form>
-                    </div>
-                    <div class="modal-footer">
-                      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                      <button type="button" class="btn btn-success">Log In</button>
+          {/* Login Modal  */}
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                  <div class="modal-dialog">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">Log In</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                      </div>
+                      <div class="modal-body">
+                        <form>
+                          <div class="form-floating mt-1 mb-3">
+                            <input type='text' class="form-control" id="floatingInput" placeholder="Username" onChange={(e)=>setUsername(e.target.value)} />
+                            <label for="floatingInput">Username</label>
+                          </div>
+                          <div class="form-floating">
+                            <input type="password" class="form-control" id="floatingPassword" placeholder="Password" onChange={(e)=>setPassword(e.target.value)} />
+                            <label for="floatingPassword" >Password</label>
+                          </div>
+                        </form>
+                      </div>
+                      <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button  class="btn btn-success" type='sumbit' data-bs-dismiss="modal" onClick={handleLogin}>Log In</button>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </div>
-
-
+            </div>
           </div>
+
+          {/* Cards Section  */}
           <div className="col-10">
-            <Cards data={data}/>
+            <Cards data={data} cardAddEnabled={cardAddEnabled}/>
           </div>
-
-
 
         </div>
     </div>
--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -6,12 +6,11 @@ import SelectedProduct from './SelectedProduct'
 
 
 function Cards(props) {
-    const {data} = props
+    const {data, cardAddEnabled} = props
     const [openCart, setOpenCart] = useState(false)
     const [selectedProduct, setSelectedProduct] = useState(0)
     const [cartList, setCartList] = useState([])
     let product = data[selectedProduct - 1]
-    console.log(data);
     function handleNewCartItem(id){
         const cartItem = {
             img: data[id].image,
@@ -33,15 +32,15 @@ function Cards(props) {
 
     return (
         <>
-
+        {/* Card items  */}
         <div className="row">
-
-
-
             { selectedProduct ? <SelectedProduct product={product}/> :
             <>
             <div className="col-10">
                 <div className='card-container d-flex flex-wrap gap-4 justify-content-center mt-4'>
+                        
+                        {/* Single Card  */}
+
                         {data.map((card)=>{
                             if(card.id == 1){
                                 card.id =1;
@@ -51,9 +50,9 @@ function Cards(props) {
                                     <img src={card.image} class="card-img-top"/>
                                     <h5 class="card-title mt-5 align-self-center">{card.title}</h5>
                                     <div class="card-body d-flex flex-column justify-content-end ">
-                                        <a onClick={()=>handleNewCartItem(card.id)} class="btn btn-primary mb-2">Add to Cart</a>
+                                        <button onClick={()=>handleNewCartItem(card.id)} class="btn btn-primary mb-2" disabled={!cardAddEnabled}>Add to Cart</button>
 
-                                        <a onClick={()=>setSelectedProduct(card.id)} class="btn btn-success">Check Out the Product</a>
+                                        <button onClick={()=>setSelectedProduct(card.id)} class="btn btn-success">Check Out the Product</button>
                                     </div>
                                 </div>
                             )
@@ -66,17 +65,21 @@ function Cards(props) {
                 {/* Cart Opener Button */}
                 <button class='cartbutton btn btn-primary  mt-4' onClick={()=>setOpenCart(!openCart)}>Cart</button>
                 {/* Cart Code */}
-                    {/* no product text  */}
-                <div class={openCart ? 'cartbox  mt-5  shadow-lg overflow-scrolloverflow-x-hidden' : 'd-none'}>
-                        {cartList.map((cartItem)=>{
+                <div class={openCart ? 'cartbox  mt-2 shadow-lg overflow-scrolloverflow-x-hidden w-100 p-3' : 'd-none'}>
+
+                       { cartList.length ?
+                        <div> {cartList.map((cartItem)=>{
                             return(
-                                <div class='d-flex position-relative justify-content-between mb-2 pe-2 pt-2 hover' onClick={()=>removeCartItem(cartItem.id)}>
+                                <div class='d-flex position-relative justify-content-between align-items-center mb-2 px-2 hover rounded' onClick={()=>removeCartItem(cartItem.id)}>
                                     <img src={cartItem.image} class='w-10'/>
                                     <p class='text-center'>{cartItem.title}</p>
-                                    <p class='text-danger'>${cartItem.price}</p>
+                                    <p class='text-danger ms-2'>${cartItem.price}</p>
                                 </div>
                             )
-                        })}
+                        })} 
+                        </div> 
+                        :
+                        <p>No items in the cart</p> }
                 </div>
 
             </div>
